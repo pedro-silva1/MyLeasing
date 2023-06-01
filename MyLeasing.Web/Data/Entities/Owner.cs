f1b2029e561ba3bacc17a27821162bf0e60ee880
ShinyNchrome <Pedro.Silva.28348@formandos.cinel.pt>
2023-06-01 20:24:11 +0100

    CRUD Owners

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 8, ErrorMessage = "Number must be between 8 and 10 digits")]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(25, ErrorMessage = "The field {0} can only contain {1} characters in length")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(25, ErrorMessage = "The field {0} can only contain {1} characters in length")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(9,ErrorMessage = "Number cannot exceed 9 digits")]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [StringLength(9, ErrorMessage = "Number cannot exceed 9 digits")]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ MyLeasing.Web/Migrations/20230601191913_ModifyOwners.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLeasing.Web.Migrations
+{
+    public partial class ModifyOwners : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Owners",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Owners",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Owners",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Owners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9,
+                oldNullable: true);
+        }
+    }
+}
