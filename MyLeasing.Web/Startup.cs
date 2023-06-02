059859f7951128adceccfd110b59dd66ec3ee76c
ShinyNchrome <Pedro.Silva.28348@formandos.cinel.pt>
2023-06-02 23:17:17 +0100

    Repository

--- MyLeasing.Web/Startup.cs
+++ MyLeasing.Web/Startup.cs
@@ -29,6 +29,8 @@ namespace MyLeasing.Web
 
             services.AddTransient<SeedDb>();
 
+            services.AddScoped<IRepository,Repository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
